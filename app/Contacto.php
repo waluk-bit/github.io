ac7a753bf312896dbfd2744d27130fa0f96c7ac0
waluk-bit <lucaslopez167@hotmail.com>
2021-06-29 20:23:11 -0300

    cambios en modelo

--- app/Contacto.php
+++ app/Contacto.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contacto extends Model
 {
-    protected $table = 'contactos';
+    protected $table = "contactos";
 
     protected $fiellable = ['id','nombre','imail','mensaje'];
 }
